ab2c830c332adcf5a752ed750be7033818cd91ba
memsharded <james@conan.io>
2017-11-23 17:21:27 +0100

    fixed tabs

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,24 @@
 from conans import ConanFile, CMake
    
 class HelloConan(ConanFile):
-   name = "Hello"
-   version = "0.1"
-   license="MIT"
-   settings = "os", "compiler", "build_type", "arch"
-   url = "https://github.com/memsharded/conan-hello.git"
+    name = "Hello"
+    version = "0.1"
+    license="MIT"
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/memsharded/conan-hello.git"
 
-   def source(self):
-       self.run("git clone https://github.com/memsharded/hello.git")
+    def source(self):
+        self.run("git clone https://github.com/memsharded/hello.git")
 
-   def build(self):
-       cmake = CMake(self)
-       self.run('cd hello && cmake . %s' % cmake.command_line)
-       self.run("cd hello && cmake --build . %s" % cmake.build_config)
+    def build(self):
+        cmake = CMake(self)
+        self.run('cd hello && cmake . %s' % cmake.command_line)
+        self.run("cd hello && cmake --build . %s" % cmake.build_config)
 
-   def package(self):
-       self.copy("*.h", dst="include", src="hello")
-       self.copy("*.lib", dst="lib", src="hello/lib")
-       self.copy("*.a", dst="lib", src="hello/lib")
+    def package(self):
+        self.copy("*.h", dst="include", src="hello")
+        self.copy("*.lib", dst="lib", src="hello/lib")
+        self.copy("*.a", dst="lib", src="hello/lib")
 
-   def package_info(self):
-       self.cpp_info.libs = ["hello"]
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
